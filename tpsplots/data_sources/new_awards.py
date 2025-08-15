f8dec824e3da439d0b842c7a217b4285e1f34082
Casey Dreier <cdreier@gmail.com>
2025-08-15 07:56:52 -0700

    Adding data source for tracking New NASA awards over time

---
+++ tpsplots/data_sources/new_awards.py
@@ -0,0 +1,8 @@
+from .google_sheets_source import GoogleSheetsSource
+
+# Example implementation
+class NewNASAAwards(GoogleSheetsSource):
+    """
+    Tracks new grants and contracts made by NASA for each month in a fiscal year
+    """
+    URL = "https://docs.google.com/spreadsheets/d/1VZZ4WAM2pVvMtUWFN9LTh0sGr0L9aO6O2eHXny28x7g/export?format=csv"
