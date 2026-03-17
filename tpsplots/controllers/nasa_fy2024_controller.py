2783d2778172b213e9a05ce51a5f4b09c2df9c40
Casey Dreier <cdreier@gmail.com>
2026-03-17 14:22:48 -0700

    Create nasa_fy2024_controller.py

---
+++ tpsplots/controllers/nasa_fy2024_controller.py
@@ -0,0 +1,19 @@
+from typing import ClassVar
+
+from tpsplots.controllers.nasa_fy_charts_controller import NASAFYChartsController
+
+
+class NASAFY2025Controller(NASAFYChartsController):
+    FISCAL_YEAR = 2024
+
+    ACCOUNTS: ClassVar[list[str]] = [
+        "Deep Space Exploration Systems",
+        "Space Operations",
+        "Space Technology",
+        "Science",
+        "Aeronautics",
+        "STEM Engagement",
+        "Safety, Security, & Mission Services",
+        "Construction & Environmental Compliance & Restoration",
+        "Inspector General",
+    ]
